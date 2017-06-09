dd84a962ca86f1cc8435a467bcb6149eb11dd600
Ola Sieradzka <ola.sieradzka93@gmail.co>
2017-06-09 16:37:00 +0200

    Initial Commit

--- client/app/scripts/controllers/main.js
+++ client/app/scripts/controllers/main.js
@@ -8,10 +8,56 @@
  * Controller of the clientApp
  */
 angular.module('clientApp')
-  .controller('MainCtrl', ['$http', 'RestEsbService', function ($http, RestEsbService) {
+  .controller('MainCtrl', ['$http', '$interval', 'RestEsbService', function ($http, $interval, RestEsbService) {
 
     RestEsbService.getUsers().then(function (data) {
       console.log(data)
     });
 
-  }]);
+    var self = this;
+    var i = 0;
+    this.labels = [];
+    this.series = ['Series A', 'Series B'];
+    this.array = []
+    this.data = [
+      this.array
+    ];
+    this.onClick = function (points, evt) {
+      console.log(points, evt);
+    };
+    this.datasetOverride = [{yAxisID: 'y-axis-1'}, {yAxisID: 'y-axis-2'}];
+    this.options = {
+      scales: {
+        yAxes: [
+          {
+            id: 'y-axis-1',
+            type: 'linear',
+            display: true,
+            position: 'left'
+          }
+        ]
+      }
+    };
+    //
+    $interval(open, 50000);
+
+    function open() {
+      var d = new Date();
+      var n = d.getHours();
+      var m = addZero(d.getMinutes());
+      var s = addZero(d.getSeconds());
+
+      i = i + 1;
+
+      self.array.push(i);
+      self.labels.push(n + ':' + m + ':' + s)
+    }
+
+    function addZero(i) {
+      if (i < 10) {
+        i = "0" + i;
+      }
+      return i;
+    }
+  }])
+;
